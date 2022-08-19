e1fb95caf422c7821747dc653f5dea7749a4face
Jeong Deokin <jjwdi0@naver.com>
2022-08-19 23:38:01 +0900

    가이드

---
+++ src/notice_crawling.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# 초기 설정
+# browser = webdriver.Chrome()
+# browser.get("URL")
+
+# 키워드 입력
+
+
+# 키워드 포함된 게시글 링크 가져오기
+
+
+# 키워드 포함된 게시글 각각 들어가기
+
+
